33d8783d66df2633f64591e1d7264fc87341858c
Gerard Banasig <tuxrace@yahoo.com>
2020-05-29 11:46:40 +0800

    Updates

---
+++ precache-manifest.4981662a486ba978b4cf333ebb7c41e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a284c8267f00a2116edb240e794aa1",
+    "url": "/react-paint-canvas/index.html"
+  },
+  {
+    "revision": "c9ab4cc973d480501c75",
+    "url": "/react-paint-canvas/static/css/main.7b593736.chunk.css"
+  },
+  {
+    "revision": "35cecf6bc0412ae8405d",
+    "url": "/react-paint-canvas/static/js/2.3a3f4aeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-paint-canvas/static/js/2.3a3f4aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9ab4cc973d480501c75",
+    "url": "/react-paint-canvas/static/js/main.ee32778f.chunk.js"
+  },
+  {
+    "revision": "30b588c0453bd18e1807",
+    "url": "/react-paint-canvas/static/js/runtime-main.66cbbe61.js"
+  }
+]);
