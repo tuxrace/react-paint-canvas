9cd5efe71dd440c70440114aeeb15ee425246af4
Gerard Banasig <tuxrace@yahoo.com>
2020-05-29 11:45:59 +0800

    Add constants

--- src/components/Shape/Shape.tsx
+++ src/components/Shape/Shape.tsx
@@ -1,4 +1,7 @@
 import React, { useLayoutEffect, useRef } from 'react'
+import { TOOLS } from '../../contants';
+
+const { SCISSOR, GLUE, ERASER } = TOOLS;
 
 type Props = {
     type: string;
@@ -11,37 +14,39 @@ const Shape: React.FC<Props> = (props) => {
     const {
         width = 200,
         height = 200,
-        pixelRatio = window.devicePixelRatio,
         type,
       } = props;
 
       const canvas = useRef<any>(null);
       
-      const drawRect = (ctx: any) => {
-        ctx.fillStyle = "green";
-        // ctx.rect(16, 16, 160, 160);
-        // ctx.fill()
-
-        // Clip a rectangular area
-        ctx.rect(16, 16, 160, 160);
-        ctx.fill();
+      const cutShape = (ctx: any) => {
         ctx.clip();
-        // Draw red rectangle after clip()
+        ctx.beginPath();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
         ctx.fillStyle = "#355BB7";
         ctx.fillRect((160 / 2), 0, 24, 180);
+      };
+
+      const removeShape = (ctx: any) => {
+        ctx.beginPath();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, width, height);
+      };
+
+      const drawRect = (ctx: any, tool: string) => {
+        ctx.fillStyle = "green";
+        ctx.rect(16, 16, 160, 160);
+        ctx.fill();
       }
 
-      const drawCircle = (ctx: any) => {
+      const drawCircle = (ctx: any, tool: string) => {
         ctx.fillStyle = "yellow";
         ctx.beginPath();
         ctx.arc(width / 2, height / 2, 80, 0, 2 * Math.PI);
         ctx.fill();
-        // Draw red rectangle after clip()
-        ctx.fillStyle = "#355BB7";
-        ctx.fillRect((width / 2), 0, 24, 180);
       }
 
-      const drawTriangle = (ctx: any) => {
+      const drawTriangle = (ctx: any, tool: string) => {
         ctx.fillStyle = "orange";
         ctx.beginPath();
         ctx.moveTo(90, 24);
@@ -49,35 +54,51 @@ const Shape: React.FC<Props> = (props) => {
         ctx.lineTo(170, 160);
         ctx.closePath();
         ctx.fill();
-        // Draw red rectangle after clip()
-        ctx.fillStyle = "#355BB7";
-        ctx.fillRect((width / 2) - 24, 0, 24, 180);
       }
 
-      const drawShape = (type: string, ctx: any) => {
+      const drawShape = (type: string, ctx: any, tool: string = 'Glue') => {
         switch(type){
             case 'rect': {
-                drawRect(ctx)
+                drawRect(ctx, tool)
+                if (tool === 'Scissor') {
+                    cutShape(ctx);
+                }
+                if (tool === 'Eraser') {
+                    removeShape(ctx);
+                }
                 break;
             }
             case 'circle': {
-                drawCircle(ctx)
+                drawCircle(ctx, tool)
+                if (tool === 'Scissor') {
+                    cutShape(ctx);
+                }
+                if (tool === 'Eraser') {
+                    removeShape(ctx);
+                }
                 break;
             }
             case 'triangle': {
-                drawTriangle(ctx)
+                drawTriangle(ctx, tool)
+                if (tool === 'Scissor') {
+                    cutShape(ctx);
+                }
+                if (tool === 'Eraser') {
+                    removeShape(ctx);
+                }
                 break;
             }
         }
       }
 
       const handleOnDrop = (e: any) => {
-        console.log(e.dataTransfer.getData("source"), type)
+        const tool = e.dataTransfer.getData("source");
+        const ctx = canvas.current.getContext("2d");
+        drawShape(type, ctx, tool);
       }
 
       useLayoutEffect(() => {
         const ctx = canvas.current.getContext("2d");
-
         ctx.save();
         drawShape(type, ctx);
       });
